dfe8f8adbd99683fc2c039d3e01ae180421b86bd
Cliff L. Biffle <code@cliffle.com>
2019-06-07 22:20:58 -0700

    Violently magenta.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
 #![no_std]
 
-#[no_mangle]
-pub extern fn the_answer() -> u32 {
-    42
-}
-
 #[panic_handler]
 fn handle_panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+const WIDTH: usize = 600;
+const HEIGHT: usize = 600;
+
+#[no_mangle]
+static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
+#[no_mangle]
+pub unsafe extern fn go() {
+    // This is called from JavaScript, and should *only* be called from
+    // JavaScript. If you maintain that condition, then we know that the &mut
+    // we're about to produce is unique, and therefore safe.
+    render_frame_safe(&mut BUFFER)
+}
+
+// We split this out so that we can escape 'unsafe' as quickly as possible.
+fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    for pixel in buffer.iter_mut() {
+        *pixel = 0xFF_FF_00_FF;
+    }
 }
