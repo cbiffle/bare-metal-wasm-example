05a3802a2efdf7a921566d20f15920a387f1d752
Cliff L. Biffle <code@cliffle.com>
2019-06-08 09:37:53 -0700

    Animate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use core::sync::atomic::{AtomicU32, Ordering};
+
 #[panic_handler]
 fn handle_panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -10,6 +12,9 @@ const HEIGHT: usize = 600;
 
 #[no_mangle]
 static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
+
+static FRAME: AtomicU32 = AtomicU32::new(0);
+
 #[no_mangle]
 pub unsafe extern fn go() {
     // This is called from JavaScript, and should *only* be called from
@@ -20,9 +25,11 @@ pub unsafe extern fn go() {
 
 // We split this out so that we can escape 'unsafe' as quickly as possible.
 fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    let f = FRAME.fetch_add(1, Ordering::Relaxed);
+
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
-            buffer[y * WIDTH + x] = (x ^ y) as u32 | 0xFF_00_00_00;
+            buffer[y * WIDTH + x] = f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
         }
     }
 }
